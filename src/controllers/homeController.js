6c136ffb663be89e451d5d5ca9e1149652564a8d
JRabbit <ledinhtho686@gmail.com>
2023-02-02 12:00:37 +0700

    sd ORM tao moi dl vao db

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,8 @@
-import db from '../models/index'
+import { response } from 'express';
+import db from '../models/index';
+import CRUDService from '../services/CRUDService';
+
+
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
@@ -19,13 +23,19 @@ let getAboutPage = (req, res) => {
     return res.render('test/about.ejs')
 }
 
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+}
 
-// object: {
-//     key: '', 
-//     value: ''
-// }
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message)
+    return res.send('post crud from server');
+}
 
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage: getAboutPage
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
 }
