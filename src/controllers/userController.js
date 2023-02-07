18a35c46d035383c587b8b57251bb1673f838dd8
JRabbit <ledinhtho686@gmail.com>
2023-02-08 01:08:33 +0700

    create api/get-all-users

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -26,6 +26,25 @@ let handleLogin = async (req, res) => {
     })
 }
 
+let handleGetAllUsers = async (req, res) => {
+    let id = req.body.id; //ALL, id
+    if (!id) {
+        return res.status(200).json({
+            errCode: 1,
+            errMessage: 'Missing required parameter',
+            users: []
+        })
+    }
+
+    let users = await userService.getAllUsers(id);
+    return res.status(200).json({
+        errCode: 0,
+        errMessage: 'Oke',
+        users
+    })
+}
+
 module.exports = {
     handleLogin: handleLogin,
+    handleGetAllUsers: handleGetAllUsers
 }
