0e71de6d865b013e6afb3b5a2edbde762ed3df80
JRabbit <ledinhtho686@gmail.com>
2023-03-14 20:17:43 +0700

    sequelize relationship and create api get detail doctor by id

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -78,8 +78,48 @@ let saveDetailInforDortor = (inputData) => {
     })
 }
 
+let getDetailDoctorById = (inputId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!inputId) {
+                resolve({
+                    errcode: 1,
+                    errMessage: 'Missing required parameter!'
+                })
+            } else {
+                let data = await db.User.findOne({
+                    where: {
+                        id: inputId
+                    },
+                    attributes: {
+                        exclude: ['password', 'image']
+                    },
+                    include: [
+                        {
+                            model: db.Markdown,
+                            attributes: ['description', 'contentHTML', 'contentMarkdown']
+                        },
+                        { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
+
+                    ],
+                    raw: true,
+                    nest: true
+                })
+
+                resolve({
+                    errCode: 0,
+                    data: data
+                })
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
-    saveDetailInforDortor: saveDetailInforDortor
+    saveDetailInforDortor: saveDetailInforDortor,
+    getDetailDoctorById: getDetailDoctorById
 }
