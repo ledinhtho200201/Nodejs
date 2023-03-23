1bb00a71263335353f1501170eb8821736e19b82
JRabbit <ledinhtho686@gmail.com>
2023-03-23 20:18:15 +0700

    76. association table: allcodes & schedules  w orm

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -162,16 +162,9 @@ let bulkCreateChedule = (data) => {
                     attributes: ['timeType', 'date', 'doctorId', 'maxNumber']
                 },
                 )
-                //convert date
-                if (existing && existing.length > 0) {
-                    existing = existing.map(item => {
-                        item.date = new Date(item.date).getTime();
-                        return item;
-                    })
-                }
                 //compare different
                 let toCreate = _.differenceWith(schedule, existing, (a, b) => {
-                    return a.timeType === b.timeType && a.date === b.date;
+                    return a.timeType === b.timeType && +a.date === +b.date;
                 });
                 //create data
                 if (toCreate && toCreate.length > 0) {
@@ -204,7 +197,13 @@ let getScheduleByDate = (doctorId, date) => {
                     where: {
                         doctorId: doctorId,
                         date: date
-                    }
+                    },
+                    include: [
+                        { model: db.Allcode, as: 'timeTypeData', attributes: ['valueEn', 'valueVi'] },
+
+                    ],
+                    raw: false,
+                    nest: true
                 })
 
                 if (!dataSchedule) dataSchedule = [];
