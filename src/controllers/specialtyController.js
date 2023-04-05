75443db380f97cc5c0cec784b992df2d8491b561
JRabbit <ledinhtho686@gmail.com>
2023-04-05 19:20:56 +0700

    94. create api: get-detail-specialty-by-id

--- src/controllers/specialtyController.js
+++ src/controllers/specialtyController.js
@@ -26,7 +26,21 @@ let getAllSpecialty = async (req, res) => {
     }
 }
 
+let getDetailSpecialtyById = async (req, res) => {
+    try {
+        let infor = await specialtyservice.getDetailSpecialtyById(req.query.id, req.query.location);
+        return res.status(200).json(infor)
+    } catch (e) {
+        console.log(e);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server...'
+        })
+    }
+}
+
 module.exports = {
     createSpecialty: createSpecialty,
-    getAllSpecialty: getAllSpecialty
+    getAllSpecialty: getAllSpecialty,
+    getDetailSpecialtyById: getDetailSpecialtyById
 }
